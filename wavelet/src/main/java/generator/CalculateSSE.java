4abe0f30e8c56795db5f9ad17140778baea5a81e
ShiboC <shibocheng33@foxmail.com>
2018-03-01 14:21:32 +0100

    experiment Update. correction:sse to long type

--- wavelet/src/main/java/generator/CalculateSSE.java
+++ wavelet/src/main/java/generator/CalculateSSE.java
@@ -24,33 +24,33 @@ public class CalculateSSE {
         DataSet<String> text = env.readTextFile(file1);
         DataSet<String> text2 = env.readTextFile(file2);
 
-        DataSet<Tuple2<String, Integer>> t1 = text.flatMap(
-                new FlatMapFunction<String, Tuple2<String, Integer>>() {
+        DataSet<Tuple2<String, Long>> t1 = text.flatMap(
+                new FlatMapFunction<String, Tuple2<String, Long>>() {
                     @Override
-                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+                    public void flatMap(String value, Collector<Tuple2<String, Long>> out) {
                         String[] tokens = value.replace("(", "").replace(")", "").split("\\W+|,");
-                        out.collect(new Tuple2<String, Integer>(tokens[0], Integer.valueOf(tokens[1])));
+                        out.collect(new Tuple2<String, Long>(tokens[0], Long.valueOf(tokens[1])));
                     }
                 }
         );
-        DataSet<Tuple2<String, Integer>> t2 = text2.flatMap(
-                new FlatMapFunction<String, Tuple2<String, Integer>>() {
+        DataSet<Tuple2<String, Long>> t2 = text2.flatMap(
+                new FlatMapFunction<String, Tuple2<String, Long>>() {
                     @Override
-                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+                    public void flatMap(String value, Collector<Tuple2<String, Long>> out) {
                         String[] tokens = value.replace("(", "").replace(")", "").split("\\W+|,");
-                        out.collect(new Tuple2<String, Integer>(tokens[0], Integer.valueOf(tokens[1])));
+                        out.collect(new Tuple2<String, Long>(tokens[0], Long.valueOf(tokens[1])));
                     }
                 }
         );
-        DataSet<Tuple2<Tuple2<String, Integer>, Tuple2<String, Integer>>> result = t1.join(t2).where(0).equalTo(0);
-        DataSet<Tuple1<Integer>> sse = result.flatMap(
-                new FlatMapFunction<Tuple2<Tuple2<String, Integer>, Tuple2<String, Integer>>, Tuple1<Integer>>() {
+        DataSet<Tuple2<Tuple2<String, Long>, Tuple2<String, Long>>> result = t1.join(t2).where(0).equalTo(0);
+        DataSet<Tuple1<Long>> sse = result.flatMap(
+                new FlatMapFunction<Tuple2<Tuple2<String, Long>, Tuple2<String, Long>>, Tuple1<Long>>() {
                     @Override
-                    public void flatMap(Tuple2<Tuple2<String, Integer>, Tuple2<String, Integer>> value, Collector<Tuple1<Integer>> out) {
-                        Integer res = (int)Math.pow(value.f0.f1 - value.f1.f1, 2);
+                    public void flatMap(Tuple2<Tuple2<String, Long>, Tuple2<String, Long>> value, Collector<Tuple1<Long>> out) {
+                        Long res = Math.round(Math.pow(value.f0.f1 - value.f1.f1, 2));
 
 
-                        out.collect(new Tuple1<Integer>(res));
+                        out.collect(new Tuple1<Long>(res));
                     }
                 }
         ).sum(0);
